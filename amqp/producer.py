f3f4a91d571042ba9743e9b256b3b9c0008f1e0f
SheiUn <me.sheiun@gmail.com>
2019-12-02 19:00:29 +0800

    add build_param func

--- amqp/producer.py
+++ amqp/producer.py
@@ -1,10 +1,20 @@
 import pika
 
 
-def produce(host="soa-rabbitmq-server"):
-    connection = pika.BlockingConnection(host=host)
+def build_param(
+    host="soa-rabbitmq-server", **kwargs
+) -> pika.ConnectionParameters:
+    return pika.ConnectionParameters(host, **kwargs)
+
+
+def produce():
+    connection = pika.BlockingConnection(build_param())
     channel = connection.channel()
     channel.basic_publish(
         exchange="test", routing_key="test", body=b"Test message."
     )
     connection.close()
+
+
+if __name__ == "__main__":
+    produce()
