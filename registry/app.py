c2eba20ca2624566ac50555e1e110adb09a264fe
SheiUn <me.sheiun@gmail.com>
2019-12-09 18:47:18 +0800

    feat: seperate views from app

--- registry/app.py
+++ registry/app.py
@@ -1,26 +1,10 @@
-from flask import Flask, jsonify, request
-from flask.views import MethodView
+from flask import Flask
 
-from controller import ServiceRegistryController
-from models import Service
-
-
-class RegistryAPI(MethodView):
-    def get(self):
-        return jsonify(ServiceRegistryController.get())
-
-    def post(self):
-        ServiceRegistryController.post(Service.from_form(request.form))
-        return self.get()
-
-    def delete(self):
-        ServiceRegistryController.delete(request.form["name"])
-        return self.get()
-
+from views import RegistryView
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-app.add_url_rule("/api/registry", view_func=RegistryAPI.as_view("registry"))
+app.add_url_rule("/api/registry", view_func=RegistryView.as_view("registry"))
 
 app.run(host="0.0.0.0")
---
+++ registry/views.py
@@ -0,0 +1,18 @@
+from flask import jsonify, request
+from flask.views import MethodView
+
+from controller import ServiceRegistryController
+from models import Service
+
+
+class RegistryView(MethodView):
+    def get(self):
+        return jsonify(ServiceRegistryController.get())
+
+    def post(self):
+        ServiceRegistryController.post(Service.from_form(request.form))
+        return self.get()
+
+    def delete(self):
+        ServiceRegistryController.delete(request.form["name"])
+        return self.get()
