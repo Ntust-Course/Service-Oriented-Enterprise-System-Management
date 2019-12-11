4fa4933310096b53cbf3b624906419183e633464
SheiUn <me.sheiun@gmail.com>
2019-12-11 14:18:15 +0800

    feat: add example service provider

---
+++ provider/app.py
@@ -0,0 +1,41 @@
+from random import randint
+
+from flask import Flask, jsonify
+
+import requests
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+INV = 10000
+
+REGISTRY_URL = "http://localhost:5000/api/registry"
+
+
+@app.route("/api/inventory", methods=["GET"])
+def inventory():
+    """simulate the current inventory"""
+    global INV
+    INV -= randint(0, 100)
+    return jsonify({"inventory": INV})
+
+
+def register():
+    res = requests.post(
+        REGISTRY_URL, {"name": "inventory", "url": "http://inventory:5000"}
+    )
+    if not res.ok:
+        raise Exception(res.reason)
+    print("=== registered === ")
+
+
+def deregister():
+    requests.delete(REGISTRY_URL, data={"name": "inventory"})
+    print("=== deregistered ===")
+
+
+try:
+    register()
+    app.run(host="0.0.0.0", port=5001)
+finally:
+    deregister()
